cc6819777b50d6cf04d1f66e52932b23949fb30b
“Ivan <ivanmelchor29@gmail.com>
2026-01-13 18:13:54 -0500

    added square, cube, and square_n_times functions

--- calculator.py
+++ calculator.py
@@ -10,6 +10,15 @@ def add(a,b):
 def subtract(a,b):
     return a - b
 
+def square(a):
+    return a ** 2
+
+def cube(a):
+    return a ** 3
+
+def square_n_times(number,n):
+    return number ** n
+
 print("I'm going use the calculator functions to multiply 5 and 6")
 x = multiply(5,6)
 print(x)
